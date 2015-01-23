a190c0044c398363e2aba8c8b1eb6f67db8bb9e9
Thura Hlaing <trhura@gmail.com>
2015-01-23 23:56:32 +0630

    added desc for prob24

--- prob24.clj
+++ prob24.clj
@@ -1,5 +1,13 @@
-(defn prob24 []
-  (nth (lexicographic-permutation (range 10)) 999999))
+;; A permutation is an ordered arrangement of objects. For example,
+;; 3124 is one possible permutation of the digits 1, 2, 3 and 4. If
+;; all of the permutations are listed numerically or alphabetically,
+;; we call it lexicographic order. The lexicographic permutations of
+;; 0, 1 and 2 are:
+
+;; 012 021 102 120 201 210
+
+;; What is the millionth lexicographic permutation of the digits 0, 1,
+;; 2, 3, 4, 5, 6, 7, 8 and 9?
 
 (defn lexicographic-permutation [coll]
   ;; http://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order
@@ -44,3 +52,6 @@
                              (recur (next-permutation cv) (conj all cv))))]
 
     (all-permutations (apply vector (sort coll)) [])))
+
+(defn prob24 []
+  (nth (lexicographic-permutation (range 10)) 999999))
