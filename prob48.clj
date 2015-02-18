5f059f8931e716fadfe746faddc2eeac31022573
Thura Hlaing <trhura@gmail.com>
2015-02-18 13:02:56 +0630

    added desc, and fix parens in prob48

--- prob48.clj
+++ prob48.clj
@@ -1,13 +1,17 @@
+;; The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
+
+;; Find the last ten digits of the series, 11 + 22 + 33 + ... +
+;; 10001000.
 
 (defn expt [x n]
   ;; expt implementation which only care about the last ten digit
   (let [multiply (fn [a b] (rem (*' a b) 10000000000))
-        square (fn [n] (multiply n n))])
+        square (fn [n] (multiply n n))]
 
-  (cond (= n 0)         1
-        (= n 1)         x
-        (even? n)       (expt (square x) (/ n 2))
-        (odd?  n)       (*' x (expt (square x) (/ (- n 1) 2)))))
+    (cond (= n 0)         1
+          (= n 1)         x
+          (even? n)       (expt (square x) (/ n 2))
+          (odd?  n)       (*' x (expt (square x) (/ (- n 1) 2))))))
 
 (defn prob48 []
   (rem (apply + (for [i (range 1 1001)]
